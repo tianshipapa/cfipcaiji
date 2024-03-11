461623da4fd8679adb2c7b89413c38d03d4f2cc7
tianshipapa <163023122+tianshipapa@users.noreply.github.com>
2024-03-12 01:45:23 +0800

    Update collect_ips.py

--- collect_ips.py
+++ collect_ips.py
@@ -3,17 +3,10 @@ from bs4 import BeautifulSoup
 import re
 import os
 
-# 目标URL
-url = 'https://monitor.gacjie.cn/page/cloudflare/ipv4.html'
-
-# 发送HTTP请求获取网页内容
-response = requests.get(url)
-
-# 使用BeautifulSoup解析HTML
-soup = BeautifulSoup(response.text, 'html.parser')
-
-# 找到所有包含IP地址的表格行
-rows = soup.find_all('tr')
+# 目标URL列表
+urls = ['https://monitor.gacjie.cn/page/cloudflare/ipv4.html', 
+        'https://ip.164746.xyz', 
+        'https://cf.090227.xyz']
 
 # 正则表达式用于匹配IP地址
 ip_pattern = r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}'
@@ -24,15 +17,28 @@ if os.path.exists('ip.txt'):
 
 # 创建一个文件来存储IP地址
 with open('ip.txt', 'w') as file:
-    for row in rows:
-        # 获取表格行中的文本内容
-        row_text = row.get_text()
+    for url in urls:
+        # 发送HTTP请求获取网页内容
+        response = requests.get(url)
         
-        # 使用正则表达式查找IP地址
-        ip_matches = re.findall(ip_pattern, row_text)
+        # 使用BeautifulSoup解析HTML
+        soup = BeautifulSoup(response.text, 'html.parser')
         
-        # 如果找到IP地址,则写入文件
-        for ip in ip_matches:
-            file.write(ip + '\n')
+        # 根据网站的不同结构找到包含IP地址的元素
+        if url == 'https://monitor.gacjie.cn/page/cloudflare/ipv4.html':
+            elements = soup.find_all('tr')
+        elif url == 'https://ip.164746.xyz':
+            elements = soup.find_all('p')
+        else:
+            elements = soup.find_all('li')
+        
+        # 遍历所有元素,查找IP地址
+        for element in elements:
+            element_text = element.get_text()
+            ip_matches = re.findall(ip_pattern, element_text)
+            
+            # 如果找到IP地址,则写入文件
+            for ip in ip_matches:
+                file.write(ip + '\n')
 
 print('IP地址已保存到ip.txt文件中。')
